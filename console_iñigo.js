15b9af1a2678c8c392d44c9fe603715275d91309
Arayaxy <inigoibarluceamoreno@gmail.com>
2024-12-16 09:30:26 +0100

    aaa

---
+++ console_iñigo.js
@@ -0,0 +1,21 @@
+// Iñigo ibarlucea Moreno
+function Sumar(num1, num2) {
+  const suma = num1 + num2;
+  console.log("El resultado de la suma es " + suma);
+}
+Sumar(6, 7);
+function Restar(num1, num2) {
+  const resta = num1 - num2;
+  console.log("El resultado de la resta es " + resta);
+}
+Restar(20, 5);
+function multiplicaccion(num1, num2) {
+  const multi = num1 * num2;
+  console.log("El resultado de la multiplicacion es " + multi);
+}
+multiplicaccion(7, 5);
+function division(num1, num2) {
+  const divi = num1 / num2;
+  console.log("El resultado de la division es " + divi);
+}
+division (36 , 6)
